35da8906c05a394d4262580e035a8c2709b705d5
Uttam Kumar <uttammaurya377@gmail.com>
2024-08-25 20:29:21 +0530

    completed

--- Recipe.js
+++ Recipe.js
@@ -3,6 +3,8 @@
 const searchBox = document.querySelector('.searchBox');
 const searchBtn = document.querySelector('.searchBtn');
 const recipeContainer = document.querySelector('.recipe-container');
+const recipeDetailsContent = document.querySelector('.recipe-details-content');
+const recipeCloseBtn = document.querySelector('.recipe-close-btn');
 
 
 // function to get recipe
@@ -47,12 +49,53 @@ const fetchRecipes = async (query) => {
             const button = document.createElement('button');
             button.textContent = "View Recipe"; 
             recipeDiv.appendChild(button);
+
+            // Adding addEventListener
+            button.addEventListener("click",()=>{
+                openRecipePopup(meal)
+            })
+
             recipeContainer.appendChild(recipeDiv);
         });
+const fetchIngredients = (meal)=>{
+        // console.log(meal)
+        let ingredientsList="";
+        for(let i=1;i<=20;i++){
+            const ingredient = meal[`strIngredient${i}`];
+            if(ingredient){
+                const measure=meal[`strMeasure${i}`];
+                ingredientsList +=`<li>${measure}${ingredient}</li>`
+            }
+            else{
+                break;
+            }
+        }
+        return ingredientsList;
+}
+const openRecipePopup =(meal)=>{
+    recipeDetailsContent.innerHTML=`
+    <h2 class="recipeName">${meal.strMeal}</h2>
+    <h3>Ingredents:</h3>
+    <ul class="ingredientList">${fetchIngredients(meal)}</ul>   
+        <div>
+    <h3 class="recipeInstructions">Instructions:</h3>
+    <p>${meal.strInstructions}</p>
+    </div>
+    `
+
+recipeDetailsContent.parentElement.style.display="block";
+}
+
+
     } catch (error) {
         console.error('Failed to fetch recipes:', error);
     }
 };
+
+
+recipeCloseBtn.addEventListener('click',()=>{
+    recipeDetailsContent.parentElement.style.display="none";
+})
 
 searchBtn.addEventListener('click', (e) => {
     // Prevents the default form submission behavior
